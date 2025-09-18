9d53920c38946e0efb966771c72fbeb1d113fa0c
anhthu258 <k.anhthu02@gmail.com>
2025-09-18 21:03:09 +0200

    idk anymore

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,16 @@ import About from "./pages/About";
 function App() {
   return (
     <section>
-    <div className="wrapper">
-      <a href="/"><img className="logo-pop" src="/assets/logo.png" alt="Anh Thu Logo" /></a>
-        <Nav />
-    </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/project" element={<Project />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <div className="wrapper">
+        <a href="/"><img className="logo-pop" src="/assets/logo.png" alt="Anh Thu Logo" /></a>
+          <Nav />
+      </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
  
     </section>
   );
