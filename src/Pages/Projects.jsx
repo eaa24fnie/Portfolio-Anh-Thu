7a923e872b776e792757a534f30a7c18dc71a851
anhthu258 <k.anhthu02@gmail.com>
2025-09-18 12:00:33 +0200

    pages fixes

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,8 +1,7 @@
 export default function Projects() {
   return (
     <section className="page">
-      <h1>Velkommen til Projects!</h1>
-      <p>Her ses mine projekter</p>
+      
     </section>
   );
 }
