7f7785699d8ba9c381c6d80e86dc95f3af62f25a
Frederik Nielsen <eaa24fnie@students.eaaa.dk>
2025-09-18 21:18:29 +0200

    TESTEST

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: "/"
-  };
-
-  // Change base path when building for production
-  if (command !== "serve") {
-    config.base = "/Portfolio/"; // 👈 Replace with your GitHub repository name
-  }
-
-  return config;
+export default defineConfig({
+  plugins: [react()],
+  base: "/Portfolio-Anh-Thu/", // 👈 important!
 });
