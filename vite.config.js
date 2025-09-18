a81b0b59a2de8b569d0320ceca0d232a0235f02a
Frederik Nielsen <eaa24fnie@students.eaaa.dk>
2025-09-18 21:34:58 +0200

    new

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio-Anh-Thu/", // 👈 must match your repo name exactly (case sensitive)
+  base: "/Portfolio-Anh-Thu/",
 });
